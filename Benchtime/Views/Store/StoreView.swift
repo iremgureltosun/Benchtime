fb1207924fbfb1a5e959cf3b58ed9c15522aff8d
Adessirem <irem.tosun@adesso.com.tr>
2024-06-24 09:44:14 +0300

    wip store api

--- Benchtime/Views/Store/StoreView.swift
+++ Benchtime/Views/Store/StoreView.swift
@@ -32,14 +32,24 @@ struct StoreView: View {
     }
 
     @ViewBuilder private func getCell(for product: Product) -> some View {
-        VStack {
-            RemoteImageView(url: product.image, contentMode: .fit)
-                .frame(width: 80, height: 80, alignment: .top)
-                .clipShape(Rectangle())
-            
-            Text(String(product.price))
-                .lineLimit(3, reservesSpace: true)
-                .font(.caption2)
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.blue.opacity(0.1))
+                .shadow(radius: 2)
+                .frame(height: 170)
+            VStack {
+                RemoteImageView(url: product.image, contentMode: .fit)
+                    .frame(width: 80, height: 80)
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 22))
+
+                Text(product.title)
+                    .lineLimit(3, reservesSpace: true)
+                    .font(.caption2)
+                
+                Text("\(String(product.price))  $")
+                    .font(.caption2)
+            }.padding(.top, 20)
         }
     }
 }
